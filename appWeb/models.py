6b54ccb9ca12e9c39ab1672129ba5a1e76567c2f
Angel G <agustavo79@gmail.com>
2017-03-21 19:06:51 -0600

    creación de modelos

--- appWeb/models.py
+++ appWeb/models.py
@@ -2,4 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Miembro(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.TextField(max_length=100)
+    edad = models.IntegerField(max_length=150)
+    idWeareble = models.IntegerField()
+
+
+class Medicamento(models.Model):
+    descripcion = models.CharField(max_length=100)
+
+
+class Grupo(models.Model):
+    idMedicamento = models.IntegerField()
+    horaInicial = models.DateTimeField()
+    periodo = models.IntegerField()
+    dosis = models.FloatField()
+    miembro = models.ForeignKey(Miembro)
+    medicamento = models.ForeignKey(Medicamento)
